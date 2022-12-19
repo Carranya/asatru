d15233e713db8400c0651c2d0b9631a334453499
Carranya <k.gong82@gmail.com>
2022-12-19 09:13:48 +0100

    create new homepage

---
+++ pages/historie.php
@@ -0,0 +1,48 @@
+<!-- <div id='historie' class='hidden'> -->
+<div id='Alemannen'>
+    <h1 class='font-bold underline text-center'>Alemannen in der Schweiz</h1>
+    <img src='img/historie/historischklein.jpg' class='float-left m-5'>
+    Im Jahr 401 sahen sich die Römer genötigt die Truppen Ihrer Grenzbefestigungen  des Rheins abzuziehen, und Ihre Provinzen schutzlos zurückzulassen.  Die Westgoten, ein germanischer Stamm, war nach Oberitalien durchgebrochen, und bedrohte die Existenz Roms.<br>
+Um diesen  begegnen zu können, wurde als letzter verzweifelter Versuch das weströmische Reich zu retten, jeder Soldat benötigt, und die Grenzen entblösst.<br>
+Im Norden lebten  zwischen Donau Main und Rhein die Alamannen. <br>
+Doch obwohl es zur Zeiten der römischen Herrschaft in der Schweiz, immer wieder militärische Auseinandersetzungen und Raubzüge gegeben hatte, setzte nach heutiger Annahme kein plötzlicher "Alamannensturm" ein. Die Archäologen sprechen von einer allmählichen Besiedelung der heutigen Deutschschweiz durch den germanischen Stamm. <br>
+In der neuen Heimat südlich des Rheins trafen die heidnischen Alemannen auf eine keltische, lateinisch sprechende , romanisierte Bevölkerung, die vornehmllich in den Städten, der christlichen Religion zugetan war. Sie vertrieben allerdings die Einheimischen nicht, und bekämpften auch nicht Ihren Glauben. Sie legten eigene Siedlungen an. Die frühen alemannischen Siedlungen sind heute anhand Ihres Namens festzustellen. Sie enden alle auf -ingen oder -ikon, wie Zofingen, Wülfingen, Wipkingen...<br><br>
+Diese Orte dürften alle vor 600 entstanden sein.<br>
+Die religiösen Verhältnisse zu dieser Zeit, auf dem Gebiet der heutigen Deutschschweiz,  müssen wir uns also so vorstellen, dass einer starken heidnischen, auf dem Land angesiedelten Mehrheit, eine geringe christliche, hauptsächlich in den Städten und Marktorten wohnende gallorömische Minderheit gegenüberstand.<br>
+Die Alemannen erlangten dadurch die Majorität. Die Sprache und Kultur setzt sich letztlich durch. Diesem Umstand ist es heute zu verdanken, dass in den nördlichen und östlichen Gebieten der heutigen Schweiz, schwyzerdütsch gesprochen wird, das von Sprachforschern zur alamannischen Sprachfamilie gezählt wird. Die Gebiete, in denen  alemannische Mundart heute noch gesprochen wird umfassen weiterhin: die südlichen 2/3 von Baden-Württemberg, westliche Teile Bayerns, Lichtenstein, Vorarlberg und im Elsass.<br>
+Nicht so, bei den Burgundern und Langobarden. Die durch die Völkerwanderung heimatlosen germanischen Stämme, siedelten sich zwar ebenfalls in Gebieten der heutigen Schweiz an. ( Burgunder 443 in der Westschweiz /  Langobarden 569 im Tessin ). Diese übernahmen allerdings die lateinische Sprache der einheimischen Bevölkerung, aus der sich im Laufe der Jahrhunderte das französische und italienische entwickelte.<br>
+    <img src='img/historie/siedlungenschweiz.jpg' class='w-[442px] text-center'>
+    <br>
+    <span class='text-red-600'>Rote Markierungen: Römische oder keltische Siedlungsgründungen</span>
+    <br>
+    <span class='text-green-600'>Grüne Markierungen: Alemannische Siedlungsgründungen</span><br><br>
+    <span class='text-xs'>
+    Quellen:<br>
+    Karte: „Die Schweiz vom Paläolithikum bis zum frühen Mittelalter“ SPM Band VI. Verlag Schweizerische Gesellschaft für Ur- und Frühgeschichte, Basel (Seite 57).<br>
+    Die Schweiz zwischen Antike und Mittelalter, Andres Furger, Verlag NZZ<br>
+    Die Alamannen, Archaeologisches Landesmuseeum Baden Wuerttemberg, Theiss Verlag<br>
+    Die Alamanen, Karin Krupp, Theiss Verlag<br>
+    www.Geschichte-Schweiz.ch<br>
+    Columban und Gallus - Urgeschichte des schweizerischen Christentums, Fritz Blanke, Fretz und Wasmuth Verlag Zürich</span>
+</div>
+<div id='Gallus'>
+    <h1 class='font-bold underline text-center'>Die Religion der heidnischen Alemannen bei Gallus und Columban</h1>
+    Die historischen Zeugnisse sind rar.<br>
+    Allerdings gewähren die Viten der Missionare Gallus und Columban einige Einblicke.<br>
+    Deren Sichtweise und Handlungen waren zwar alles andere als wohlmeinend - doch können wir einiges entnehmen.<br>
+    Zunächst reisten die beiden Missionare 610 n.d.Z. nach Tuggen am Zürichsee, wo sie einen oder mehrere Tempel der Alemannen vorfanden ! Es wird davon ausgegangen, dass diese Holzbauten waren, und so steckten sie einen in Brand, um die Überlegenheit ihres Gottes zu beweisen. Die Opfergaben warfen sie in den See. Daher müssten sie aus Metall gewesen sein. Die Tuggener allerdings liessen sich nicht auf diese unfreundliche Art bekehren, sondern beschlossen auf einem Thing Gallus nach dem Leben zu trachten und Columban zu vertreiben.Auf der Flucht verfluchten sie die Tuggener Bevölkerung.<br>
+    Nach einem Zwischenstopp in Arbon, reisten sie weiter nach Bregenz wo sie in einer ehemaligen Kirche 3 vergoldete, eherne heidnische Götterbilder vorfanden, die von der einheimischen Bevölkerung dort als "Hauptgötter" verehrt wurden. Leider ist hier nicht überliefert, um welche Götter es sich gehandelt hat. Es gibt lediglich Vermutungen, dass es sich um Wodan, Donar und Ziu gehandelt haben könnte.<br>
+    Auch hier zerschmettern die Missionare die Götterbildnisse, und werfen die Stücke in den Bodensee.<br>
+    Columban trifft später dann Heiden an, die ein Bieropfer für Wodan in einem Kessel vorbereiten. Glücklicherweise wird der Name des Gottes genannt, und so können wir belegen, dass Wodan von den Alemannen verehrt wurde.<br>
+    Auch hier zerstört Columban den Opferkessel.<br>
+    In der Vita  nach Wetti begegnen die Missionare während ihrer Reise auch Wesen aus der heidnischen "sekundären Mythologie" - Naturgeistern, wie Wassergeistern, Nixen und Berggeistern, aber auch Alb, Schrat oder Nachtmahr.<br>
+    Die Nixen werden als Schutzgeister der Fische dargestellt.<br>
+    Aus dem Lebensbericht der Missionare kann man ersehen, dass sie zwar rücksichtslos gegen den heidnischen Glauben vorgingen, aber die Existenz der germanischen Götter und Geister nicht leugneten.<br>
+    Ferner berichtet etwa zur Mitte des 6. Jahrhunderts der byzantinische Geschichtsschreiber Agathias aus Myrina in seinem Bericht über den Krieg der Franken und Goten gegen die Byzantiner, daß sich die im Heer des Frankenkönigs Theudebald mitkämpfenden Alamannen lediglich in Bezug auf ihre Religion von den Franken unterscheiden, da sie "irgendwelche Bäume und Flüsse, Hügel und Schluchten wie Götter verehren."<br><br>
+    <span class='text-xs'>
+    Quellen:<br>
+    Columban und Gallus - Urgeschichte des schweizerischen Christentums, Fritz Blanke, Fretz und Wasmuth Verlag Zürich<br>
+    Römer -Heilige-Alemannen im Zürichbiet, v. A.Tanner<br>
+    <a href='http://www.archaeologie-online.de/magazin/thema/die-alamannen/'>http://www.archaeologie-online.de/magazin/thema/die-alamannen/</a>
+    </span>
+</div>

0626457ce744ba9e957c63c9a0f7dee69bafbd4a
Carranya <k.gong82@gmail.com>
2022-12-19 09:13:53 +0100

    create new homepage

---
+++ pages/navigation.php
@@ -0,0 +1,14 @@
+<div class='flex justify-center bg-gradient-to-r from-lime-600 to-lime-800 text-white font-bold text-center'>
+    <div id='menuHome' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php'><span class='pl-5 pr-5'>Home</span></a></div>
+    <div id='menuGlaube' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=glaube'><span class='pl-5 pr-5'>Unser Glaube</span></a></div>
+    <div id='menuTips' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=tips'><span class='pl-5 pr-5'>Tips & Links</span></a></div>
+    <div id='menuHistorie' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=historie'><span class='pl-5 pr-5'>Historisches</span></a>
+        <div id='subHistorie'class='hidden text-center'>
+            <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><span id='subAlemannen' >Alemannen in der Schweiz</span></div>
+            <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><span id='subGallus'>Die Religion der heidnischen Alemannen bei Gallus und Columban</span></div>
+        </div>
+    </div>
+    <div id='menuVerein' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=verein'><span class='pl-5 pr-5'>Verein Asatru Schweiz</span></a></div>
+    <div id='menuStammtisch' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=stammtisch'><span class='pl-5 pr-5'>Stammtisch</span></a></div>
+    <div id='menuForum' class='pt-3 pb-3 hover:bg-lime-600'><a href='#'><span class='pl-5 pr-5'>Forum</span></a></div>
+</div>

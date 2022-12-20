607ebf8842fc58035aa9ecf6b16aede87b9a02e6
Carranya <k.gong82@gmail.com>
2022-12-20 09:07:40 +0100

    create pages

--- pages/glaube.php
+++ pages/glaube.php
@@ -1,1 +1,83 @@
-Glaube
+<div id='goetter'>
+    <h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-5 mb-5 p-2'>Unsere Götter</h1>
+    <img src='img/glaube/natur3.jpg' class='float-left m-5 w-60'>
+    Die wichtigsten Hinweise über die Götter lassen sich einerseits sicher in den alten Quellen ( Edda, Merseburger Zaubersprüche u.a ) finden, anderseits sind alte Bräuche und Sagen in dieser Hinsicht aufschlussreich. Vieles heidnische hat die Jahrhunderte in ganz Europa überdauert, teils umgedeutet in christlicher Hinsicht, teils in alten Ritualen, Namen und alten Geschichten. Wir möchten bewusst nicht zwischen kontinentalgermanischem und nordisch-germanischen Asatru trennen. Die Götter mögen in verschiedenen Gegenden verschiedene Nuancen besessen haben, doch sind es die gleichen! Auch Juden, Christen und Muslime interpretieren und verehren den gleichen Gottauf unterschiedliche Weise..<br><br>
+    Alle Quellen und Bräuche sind für uns von Interesse, auch wenn lokale unser besonderes Interesse finden. Letztlich versuchen wir aber nicht einen heidnischen Glauben nachzuahmen, der schon vor vielen Jahrhunderten von der Bildfläche verschwand. Sondern wir verstehen uns als moderne Menschen, die sich die Freiheit nehmen, Ihren Glauben der heutigen Zeit und Sichtweisen anzupassen.
+    <div class='flex flex-wrap justify-center'>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=glaube#gott1'>Odin/Wotan/Wuodan</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=glaube#gott2'>Frigga/Frigg</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=glaube#gott3'>Thor/Donar</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=glaube#gott4'>Tyr/Ziu</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=glaube#gott5'>Balder/Baldur/Phol</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=glaube#gott6'>Freya</a></div>
+    </div>
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnGlaube.php'; ?>
+    </div>
+</div>
+<div id='gott1'>
+    <h1 class='font-bold text-base text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-3 mb-3 p-2'>Odin/Wotan/Wuodan</h1>   
+    Nicht nur die Edda erzählt uns von Ihm, sondern auch in alt her überlieferten Sagen und Geschichten. Odin ist eine vielschichtige Gottheit mit vielen Facetten und Gesichtern. "Wodan id est furor " so berichtete einst der Missionar Adam von Bremen. Übersetzt wird das oft als "Odin ist Wut". Einen ähnlichen Begriff "furore machen" gibt es noch im heutigen Gebrauch. Wikipedia meint dazu :"Die Ableitung des italienischen  Ausdrucks Furore für rasenden Beifall sowie Leidenschaftlichkeit wird im Zusammenhang mit Furore machen als großes Aufsehen erregen und Beifall erringen definiert." Wir würden es dann (unwissenschaftlich) auch so übersetzen "Odin ist Extase".<br>
+    Er verfolgt kompromisslos sein Ziel, und fordert viel von denen die Ihm folgen. Auf der Suche nach Erkenntnis opfert er sein Auge für einen Schluck aus Mirmirs Brunnen der Weisheit. Mehr noch - er hängt sich 9 Tage und Nächte an den Weltenbaum, opfert sich letztlich mit dem Speer selbst, und findet so die Runen.<br>
+    Er ist als einziger männlicher Gott als zauberkundig bekannt . Von Freya laesst er sich die Seidh - Kunst lehren.<br>
+    Er ist der Gott, der vor der Schlacht um den Sieg angerufen wurde.<br>
+    Aber ebenso ist er der Gott der Poesie. Er entfuehrt den Odroerir, den Dichtermet. Der Legende nach wurde derjenige der davon trank zum Dichter.<br>
+    Als  Sturmgott wird über Ihn auch aus der Schweiz als Anführer des Wuotisheers (der wilden Jagd) berichtet.<br>
+    Als einsamer Wanderer zieht er durch Mittgard, die Welt der Menschen.<br>
+    Seine über 150 bekannten Namen geben  Ausdruck seiner Vielschichtigkeit. 
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnGlaube.php'; ?>
+    </div>
+</div>
+<div id='gott2'>
+    <h1 class='font-bold text-base text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-3 mb-3 p-2'>Frigga/Frigg</h1>   
+    Sie ist die Gemahlin Odins. Sie bietet in den Mythen als mächtigste der Asinnen selbst Odin die Stirn. So in der Sage um die Namensgebung der Langobarden, in der sie Ihren Gatten überlistet, und so den Langobarden den Sieg schenkt, den Odin für die Gegner vorgesehen hatte. Sie ist Schutzherrin der Ehe und Mutterschaft.
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnGlaube.php'; ?>
+    </div>
+</div>
+<div id='gott3'>
+    <h1 class='font-bold text-base text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-3 mb-3 p-2'>Thor/Donar</h1>   
+    Der Donnerer, Wettergott und Beschützer der Bauern und des einfachen Volkes. Indem er mit seinem Hammer gegen Riesen streitet, hält er das empfindliche Gleichgewicht der Natur in der Waage. Er ist aufgrund dessen einer der populärsten Götter. Nicht umsonst ist sein Hammer eines der wichtigsten Symbole unseres Glaubens. Im südgermanischen Raum findet man oft in weiblichen Gräbern die Donarskeule als Grabbeigabe. 
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnGlaube.php'; ?>
+    </div>
+</div>
+<div id='gott4'>
+    <h1 class='font-bold text-base text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-3 mb-3 p-2'>Tyr/Ziu</h1>   
+    Bei Mittel und Südgermanen heisst er Ziu, Tsiu althochdeutsch Teiwaz oder Tiwaz.<br>
+    Tacitus berichtet, dass Pferde ihm geopfert wurden. Tapferster und kühnster unter den Asen, Gott des Krieges und des Zweikampfes wird er genannt. Bei den Südgermanen, besonders bei den Alemannen war er der wichtigste Gott. Er legt als Pfand seine Hand in Fenrirs Maul, damit der Wolf gefesselt werden kann, die ihm daraufhin abgebissen wird. Die Einarmigkeit lässt sich auch bei anderen Göttern des Rechts feststellen. Der lokale Name des Dienstag - der Ziischtig, macht uns heute noch deutlich, dass dieser Tag Ziu gewidmet ist. Er gilt als Beschützer des Things ( der Stammesversammlung ). Er legt als Pfand seine Hand in Fenrirs Maul, damit der Wolf gefesselt werden kann, die ihm daraufhin abgebissen wird. Durch sein Opfer der Hand, gibt er Gemeinschaft die Möglichkeit die Gefahr, die von einem freien Fenrirswolf ausgeht, zu bannen.
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnGlaube.php'; ?>
+    </div>
+</div>
+<div id='gott5'>
+    <h1 class='font-bold text-base text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-3 mb-3 p-2'>Balder/Baldur/Phol</h1>   
+    Sohn Odins und Frigg. Er steht für Reinheit, Licht, Schönheit und Güte. Baldur gilt als Odins und Friggas Lieblingssohn. In der Mythologie taeuscht der listige Loki Baldurs blinden Bruder Hödur, und erreicht so dass Hödur Balder tötet. Hermod versucht vergelblich ihn aus dem Totenreich von Hel zurückzuholen. Doch kehrt Baldur nach Ragnarök zurück, und läutet mit den anderen Überlebenden ein goldenes Zeitalter ein. Da Baldur auch für die Sonne steht, symbolisiert sein Schicksal den Jahreskreis - den ewigen Zyklus von Tod und Wiedergeburt. Baldur ist neben in der Edda auch in den Merseburger Zaubersprüchen belegt. 
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnGlaube.php'; ?>
+    </div>
+</div>
+<div id='gott6'>
+    <h1 class='font-bold text-base text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-3 mb-3 p-2'>Freya</h1>   
+    Einige schweizer Sagengestalten werden mit Ihr in Verbindung gebracht, so z.B. die Sträggele, die als "Nachfolgerin der Freya" in Erzählungen bis heute überlebt hat. . Die Römer setzten sie mit Ihrer Göttin Venus gleich. Der berauschende Hanf galt als Ihre Pflanze. Sie ist aber weit mehr als die Göttin der Liebe und Sehnsucht. Sie lehrt Odin Magie und besitzt als Oberste der Walküren auch kämpferische Aspekte. Extase, wird von mir ebenfalls für Sie als passend empfunden, als Parallele zu Odin.
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnGlaube.php'; ?>
+    </div>
+</div>
+<span class='text-xs'>
+    Natürlich umfasst unser Pantheon weitaus mehr Göttinnen und Götter. Dies ist als eine persönliche Sichtweise und Auswahl zu verstehen und stellt keine allgemeinverbindliche Wertung dar. Über Ergänzungen würde ich mich freuen.
+</span>
+
+<div id='naturreligion' class='mt-32'>
+    <h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-5 mb-5 p-2'>Naturreligion</h1>
+    <img src='img/glaube/trommelrunen.jpg' class='float-left m-5 w-96'>
+    Zunächst gilt gilt es mit einem Missverständnis aufzuräumen.In unserem Verständnis wird in einer Naturreligion nicht die Natur selbst als Gottheit verehrt, sondern die Natur als beseelt erlebt. Die Natur als solche begreifen wir als unseren Ursprung und Ziel, Ihre Gesetze und Regeln bilden die Grundlage allen Lebens. Wir sind "nur" ein Teil des Ganzen - unser Sein und Schicksal folgt dem ganzen. Selbst unsere Götter sind diesen Gesetzmässigkeiten unterworfen und können diese auch nicht ohne weiteres  für sich selbst ändern.<br>
+    So konnten Frigga und Odin, obwohl sie es vorher sehen konnten, nicht verhindern das Ihr Sohn Balder letztlich durch Loki den Tod erleidete und ins Totenreich zu Hel ging. Dies ist ein prägnanter Unterschied zu monotheistischen Religionen, in denen der einzige Gott allmächtig ist und nicht ein Schicksal erleidet sondern dieses selbst für alles Lebendige bestimmt.<br>
+    Die sogenannte "sekundäre Mythologie" nennt uns die Geister der Natur beim Namen. Elfen, Zwerge und Riesen sind für uns nicht nur Sinnbilder der Naturkräfte.<br>
+    Auch unsere Ahnen sind in diesem Zusammenhang gegenwärtig und Ihre Verehrung macht uns unsere Wurzeln und Herkunft bewusst und geben uns Halt im Alltag.<br>
+    Der zyklische Kreislauf der Natur - Leben und Sterben - ist auch unser persönliches unausweichliches Schicksal.
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnGlaube.php'; ?>
+    </div>
+</div>

2134f3070df58021506796c0e5b20f335db26373
Carranya <k.gong82@gmail.com>
2022-12-20 09:07:44 +0100

    create pages

--- pages/historie.php
+++ pages/historie.php
@@ -1,15 +1,15 @@
 <div id='alemannen'>
-    <h1 class='font-bold underline text-center'>Alemannen in der Schweiz</h1>
+    <h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-5 mb-5 p-2'>Alemannen in der Schweiz</h1>
     <img src='img/historie/historischklein.jpg' class='float-left m-5'>
     Im Jahr 401 sahen sich die Römer genötigt die Truppen Ihrer Grenzbefestigungen  des Rheins abzuziehen, und Ihre Provinzen schutzlos zurückzulassen.  Die Westgoten, ein germanischer Stamm, war nach Oberitalien durchgebrochen, und bedrohte die Existenz Roms.<br>
-Um diesen  begegnen zu können, wurde als letzter verzweifelter Versuch das weströmische Reich zu retten, jeder Soldat benötigt, und die Grenzen entblösst.<br>
-Im Norden lebten  zwischen Donau Main und Rhein die Alamannen. <br>
-Doch obwohl es zur Zeiten der römischen Herrschaft in der Schweiz, immer wieder militärische Auseinandersetzungen und Raubzüge gegeben hatte, setzte nach heutiger Annahme kein plötzlicher "Alamannensturm" ein. Die Archäologen sprechen von einer allmählichen Besiedelung der heutigen Deutschschweiz durch den germanischen Stamm. <br>
-In der neuen Heimat südlich des Rheins trafen die heidnischen Alemannen auf eine keltische, lateinisch sprechende , romanisierte Bevölkerung, die vornehmllich in den Städten, der christlichen Religion zugetan war. Sie vertrieben allerdings die Einheimischen nicht, und bekämpften auch nicht Ihren Glauben. Sie legten eigene Siedlungen an. Die frühen alemannischen Siedlungen sind heute anhand Ihres Namens festzustellen. Sie enden alle auf -ingen oder -ikon, wie Zofingen, Wülfingen, Wipkingen...<br><br>
-Diese Orte dürften alle vor 600 entstanden sein.<br>
-Die religiösen Verhältnisse zu dieser Zeit, auf dem Gebiet der heutigen Deutschschweiz,  müssen wir uns also so vorstellen, dass einer starken heidnischen, auf dem Land angesiedelten Mehrheit, eine geringe christliche, hauptsächlich in den Städten und Marktorten wohnende gallorömische Minderheit gegenüberstand.<br>
-Die Alemannen erlangten dadurch die Majorität. Die Sprache und Kultur setzt sich letztlich durch. Diesem Umstand ist es heute zu verdanken, dass in den nördlichen und östlichen Gebieten der heutigen Schweiz, schwyzerdütsch gesprochen wird, das von Sprachforschern zur alamannischen Sprachfamilie gezählt wird. Die Gebiete, in denen  alemannische Mundart heute noch gesprochen wird umfassen weiterhin: die südlichen 2/3 von Baden-Württemberg, westliche Teile Bayerns, Lichtenstein, Vorarlberg und im Elsass.<br>
-Nicht so, bei den Burgundern und Langobarden. Die durch die Völkerwanderung heimatlosen germanischen Stämme, siedelten sich zwar ebenfalls in Gebieten der heutigen Schweiz an. ( Burgunder 443 in der Westschweiz /  Langobarden 569 im Tessin ). Diese übernahmen allerdings die lateinische Sprache der einheimischen Bevölkerung, aus der sich im Laufe der Jahrhunderte das französische und italienische entwickelte.<br>
+    Um diesen  begegnen zu können, wurde als letzter verzweifelter Versuch das weströmische Reich zu retten, jeder Soldat benötigt, und die Grenzen entblösst.<br>
+    Im Norden lebten  zwischen Donau Main und Rhein die Alamannen. <br>
+    Doch obwohl es zur Zeiten der römischen Herrschaft in der Schweiz, immer wieder militärische Auseinandersetzungen und Raubzüge gegeben hatte, setzte nach heutiger Annahme kein plötzlicher "Alamannensturm" ein. Die Archäologen sprechen von einer allmählichen Besiedelung der heutigen Deutschschweiz durch den germanischen Stamm. <br>
+    In der neuen Heimat südlich des Rheins trafen die heidnischen Alemannen auf eine keltische, lateinisch sprechende , romanisierte Bevölkerung, die vornehmllich in den Städten, der christlichen Religion zugetan war. Sie vertrieben allerdings die Einheimischen nicht, und bekämpften auch nicht Ihren Glauben. Sie legten eigene Siedlungen an. Die frühen alemannischen Siedlungen sind heute anhand Ihres Namens festzustellen. Sie enden alle auf -ingen oder -ikon, wie Zofingen, Wülfingen, Wipkingen...<br><br>
+    Diese Orte dürften alle vor 600 entstanden sein.<br>
+    Die religiösen Verhältnisse zu dieser Zeit, auf dem Gebiet der heutigen Deutschschweiz,  müssen wir uns also so vorstellen, dass einer starken heidnischen, auf dem Land angesiedelten Mehrheit, eine geringe christliche, hauptsächlich in den Städten und Marktorten wohnende gallorömische Minderheit gegenüberstand.<br>
+    Die Alemannen erlangten dadurch die Majorität. Die Sprache und Kultur setzt sich letztlich durch. Diesem Umstand ist es heute zu verdanken, dass in den nördlichen und östlichen Gebieten der heutigen Schweiz, schwyzerdütsch gesprochen wird, das von Sprachforschern zur alamannischen Sprachfamilie gezählt wird. Die Gebiete, in denen  alemannische Mundart heute noch gesprochen wird umfassen weiterhin: die südlichen 2/3 von Baden-Württemberg, westliche Teile Bayerns, Lichtenstein, Vorarlberg und im Elsass.<br>
+    Nicht so, bei den Burgundern und Langobarden. Die durch die Völkerwanderung heimatlosen germanischen Stämme, siedelten sich zwar ebenfalls in Gebieten der heutigen Schweiz an. ( Burgunder 443 in der Westschweiz /  Langobarden 569 im Tessin ). Diese übernahmen allerdings die lateinische Sprache der einheimischen Bevölkerung, aus der sich im Laufe der Jahrhunderte das französische und italienische entwickelte.<br>
     <img src='img/historie/siedlungenschweiz.jpg' class='w-[442px] text-center'>
     <br>
     <span class='text-red-600'>Rote Markierungen: Römische oder keltische Siedlungsgründungen</span>
@@ -22,10 +22,14 @@ Nicht so, bei den Burgundern und Langobarden. Die durch die Völkerwanderung hei
     Die Alamannen, Archaeologisches Landesmuseeum Baden Wuerttemberg, Theiss Verlag<br>
     Die Alamanen, Karin Krupp, Theiss Verlag<br>
     www.Geschichte-Schweiz.ch<br>
-    Columban und Gallus - Urgeschichte des schweizerischen Christentums, Fritz Blanke, Fretz und Wasmuth Verlag Zürich</span>
+    Columban und Gallus - Urgeschichte des schweizerischen Christentums, Fritz Blanke, Fretz und Wasmuth Verlag Zürich
+    </span>
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnHistorie.php'; ?>
+    </div>
 </div>
 <div id='gallus'>
-    <h1 class='font-bold underline text-center'>Die Religion der heidnischen Alemannen bei Gallus und Columban</h1>
+    <h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-5 mb-5 p-2'>Die Religion der heidnischen Alemannen bei Gallus und Columban</h1>
     Die historischen Zeugnisse sind rar.<br>
     Allerdings gewähren die Viten der Missionare Gallus und Columban einige Einblicke.<br>
     Deren Sichtweise und Handlungen waren zwar alles andere als wohlmeinend - doch können wir einiges entnehmen.<br>
@@ -44,4 +48,47 @@ Nicht so, bei den Burgundern und Langobarden. Die durch die Völkerwanderung hei
     Römer -Heilige-Alemannen im Zürichbiet, v. A.Tanner<br>
     <a href='http://www.archaeologie-online.de/magazin/thema/die-alamannen/'>http://www.archaeologie-online.de/magazin/thema/die-alamannen/</a>
     </span>
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnHistorie.php'; ?>
+    </div>
+</div>
+<div id='arch'>
+    <h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-5 mb-5 p-2'>Archäologische Funde</h1>    
+    <div class='flex flex-wrap justify-center'>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=historie#arch1'>Schwertscheide bei Gutenstein</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=historie#arch2'>Grab in Kaiseraugst mit 2 Grabsteinen</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=historie#arch3'>Bügelfibel aus Hüfingen</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=historie#arch4'>Scheibenfibel von Bülach</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=historie#arch5'>Runenspange von Nordendorf</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=historie#arch6'>Scheidenmundblech eines Spatha</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=historie#arch7'>Columban und die Götterbildnisse</a></div>
+        <div class='text-white pt-3 pb-3 pl-5 pr-5 bg-lime-600 hover:bg-lime-500 border-white border-solid border-2'><a href='index.php?page=historie#arch8'>Reiterscheibe von Pliezhausen</a></div>
+    </div>
+    <div class='flex justify-end'>
+        <?php include 'pages/global/returnHistorie.php'; ?>
+    </div>
+</div>
+<div id='arch1'>
+    <h1 class='font-bold text-base text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-3 mb-3 p-2'>Schwertscheide bei Gutenstein</h1>    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 </div>

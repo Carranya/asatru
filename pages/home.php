09436adf4be6b3a0e6a1860525e470013f9960c0
Carranya <k.gong82@gmail.com>
2022-12-20 09:07:47 +0100

    create pages

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,15 @@
-Home
+<div>
+<h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white m-5 p-2'>Willkommen bei AsatruSchweiz.ch</h1>
+    <img src='img/home/index1.jpg' class='float-left m-5 w-48'>
+    Wir sind eine Gemeinschaft von Asatruar, die Ihren Glauben gemeinsam im Alltag unserer technisierten und globalisierten Welt leben möchte. Aber warum diese Internetseite?<br>
+    Oft entdecken Menschen den "alten Weg des Glaubens" für sich alleine, in Ihren Gedanken, Gefühlen und Träumen - und fernab einer religiösen Gemeinschaft bleibt man schnell allein zurück.  Viele Fragen bleiben, selbst nach längerem Studium diverser Literatur. Und auch nach dem Besuch vieler Foren sehnen wir uns nach einer Gemeinschaft gleichgesinnter, wo wir unsere Fragen und Gedanken frei äussern, und unseren Glauben leben können. Diese Internetseite soll ein Angebot sein. Eine Einladung uns anzuschreiben,  zu fragen,  zu treffen... und vielleicht vieles mehr.<br>
+    Aber erst der Reihe nach - Wir haben uns vorgenommen, auf jede Mail in diesem Zusammenhang zu antworten! Wir werden sehen - streng nach dem Motto nichts muss - alles kann.<br>
+    Na dann los - wir freuen uns.
+</div>
+
+<h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white m-5 p-2'>Asatru</h1>
+    <img src='img/home/holzhammer.jpg' class='float-left m-5 w-32'>
+    Asatru - der Glaube der sich an der germanisch/nordischen Mythologie orientiert - dem Glauben an die Asen und Wanen. Eine mit den Traditionen und überlieferten Quellen verbundene Rekonstruktion eines alten Glaubens, die aber im hier und jetzt des 21. Jahrhundert stattfindet.  Obwohl die Edda für uns eine der wichtigsten Quellen darstellt, ist Asatru keine Buchreligion, sondern massgebend ist die persönliche Erfahrung jedes einzelnen mit den Göttern.<br>
+    In diesem Sinn ist die Edda keine "Bibel", sondern ermöglicht lediglich eine wichtige Retroperspektive.<br>
+    "Die Götter findet man nicht zwischen zwei Buchdeckeln" Dieser Satz eines Freundes bringt dies treffend zum Ausdruck. So sehr wichtig Kenntnisse der alten Quellen auch sind, Glaube ist letztlich erlebte Spiritualität und somit Emotion.
+</div>

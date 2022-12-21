0ca635092332e2a824beb0eeb3c6dfca653b7d9f
Carranya <k.gong82@gmail.com>
2022-12-21 18:45:50 +0100

    new logo

---
+++ pages/global/logo.php
@@ -0,0 +1,4 @@
+<div class='hidden md:block absolute top-5 left-10 z-10'><a href='index.php'><img src='img/logo.gif' class='w-52'></a></div>
+<div class='md:hidden flex justify-center'>
+    <img src='img/logo.gif' class='w-24'>
+</div>

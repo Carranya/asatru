35907f9f3c3589695ecde8f8bdd976a28bccf281
Carranya <k.gong82@gmail.com>
2022-12-21 18:46:06 +0100

    modified headers

--- pages/global/navigation.php
+++ pages/global/navigation.php
@@ -1,22 +1,25 @@
-<form action='index.php' method='post'>
-<div class='flex justify-center bg-gradient-to-r from-lime-600 to-lime-800 text-white font-bold text-center'>
-    <div id='menuHome' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php'><span class='pl-5 pr-5'>Home</span></a></div>
-    <div id='menuGlaube' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=glaube'><span class='pl-5 pr-5'>Unser Glaube</span></a>
-        <div id='subGlaube' class='hidden text-center'>
-            <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=glaube#goetter'> Unsere Götter</a></div>
-            <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=glaube#naturreligion'>Naturreligion</a></div>
+<div id='openNav' class='md:hidden flex justify-center w-full bg-gradient-to-r from-lime-600 to-lime-800'><img src='img/icons/menu.png' class='w-10'></div>
+<div id='closeNav' class='hidden md:hidden justify-center w-full bg-gradient-to-r from-lime-600 to-lime-800'><img src='img/icons/close.png' class='w-10'></div>
+<div id='nav' class='hidden md:block sticky top-0'>
+    <div class='md:flex justify-center items-center bg-gradient-to-r from-lime-600 to-lime-800 text-white font-bold text-center'>
+        <div id='menuHome' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=home'><span class='pl-5 pr-5'>Home</span></a></div>
+        <div id='menuGlaube' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=glaube'><span class='pl-5 pr-5'>Unser Glaube</span></a>
+            <div id='subGlaube' class='hidden text-center'>
+                <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=glaube#goetter'> Unsere Götter</a></div>
+                <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=glaube#naturreligion'>Naturreligion</a></div>
+            </div>
         </div>
-    </div>
-    <div id='menuTips' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=tips'><span class='pl-5 pr-5'>Tips & Links</span></a></div>
-    <div id='menuHistorie' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=historie'><span class='pl-5 pr-5'>Historisches</span></a>
-        <div id='subHistorie' class='hidden text-center'>
-            <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#alemannen'>Alemannen in CH</a></div>
-            <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#gallus'>Die Religion der heid...</a></div>
-            <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#arch'>Archäologische Funde</a></div>
+        <div id='menuTips' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=tips'><span class='pl-5 pr-5'>Tips & Links</span></a></div>
+        <div id='menuHistorie' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=historie'><span class='pl-5 pr-5'>Historisches</span></a>
+            <div id='subHistorie' class='hidden text-center'>
+                <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#alemannen'>Alemannen in CH</a></div>
+                <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#gallus'>Die Religion der heid...</a></div>
+                <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#arch'>Archäologische Funde</a></div>
+            </div>
         </div>
+        <div id='menuVerein' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=verein'><span class='pl-5 pr-5'>Verein Asatru Schweiz</span></a></div>
+        <div id='menuStammtisch' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=stammtisch'><span class='pl-5 pr-5'>Stammtisch</span></a></div>
+        <div id='menuForum' class='pt-3 pb-3 hover:bg-lime-600'><a href='#'><span class='pl-5 pr-5'>Forum</span></a></div>
+        <div class='pt-3 pb-3 ml-3'><a href='#end'><img src='img/icons/expand.png' class='w-7 bg-white shadow rounded-full hover:opacity-40'></a></div>
     </div>
-    <div id='menuVerein' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=verein'><span class='pl-5 pr-5'>Verein Asatru Schweiz</span></a></div>
-    <div id='menuStammtisch' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=stammtisch'><span class='pl-5 pr-5'>Stammtisch</span></a></div>
-    <div id='menuForum' class='pt-3 pb-3 hover:bg-lime-600'><a href='#'><span class='pl-5 pr-5'>Forum</span></a></div>
 </div>
-</form>

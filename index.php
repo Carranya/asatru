5aa317cbc5421a31dff60ffda82e30bc06d2cde2
Carranya <k.gong82@gmail.com>
2022-12-21 18:44:18 +0100

    new navigation

--- index.php
+++ index.php
@@ -6,25 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
     <link rel="stylesheet" href="css/output.css">
+    <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="js/navigation.js"></script>
 </head>
 <body class='bg-gradient-to-t from-lime-100 to-lime-600'>
     <?php require ('functions/pageFunctions.php'); ?>
+    <!-- Logo -->
+    <?php include 'pages/global/logo.php'; ?>
+    
     <!-- Banner -->
     <div id='banner'>
-        <img src='img/header.gif' class='w-full h-40 object-cover'>
+        <?php 
+            getBanner(@$_GET['page']);
+        ?>
     </div>
     <!-- Navigation -->
-
+    <div id='top'></div>
     <?php include 'pages/global/navigation.php'; ?>
-
     <div class='flex justify-center text-justify'>
-        <div class='w-1/2'>
+        <div class='w-full p-5 md:w-1/2'>
             <?php 
                 getPage(@$_GET['page']);
             ?>
         </div>
     </div>
 </div>
+<div id='end'></div>
 </body>
 </html>

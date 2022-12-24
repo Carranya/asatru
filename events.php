84266eff146f2292203d112d8182062188e68af4
Carranya <k.gong82@gmail.com>
2022-12-24 21:57:07 +0100

    add edit functions events

--- events.php
+++ events.php
@@ -2,18 +2,29 @@
 
 [
 'id' => 1,
-'title' => 'Stammtisch',
-'date' => '2022-12-24',
-'time' => '18:00',
-'location' => 'St. Gallerhof Zürich',
+'title' => 'Feier5',
+'date' => '2022-12-15',
+'time' => '21:00',
+'location' => 'Muttenz',
+'deleted' => '',
 ],
 
 [
 'id' => 2,
 'title' => 'Stammtisch',
-'date' => '2022-12-25',
+'date' => '2022-12-29',
 'time' => '18:00',
 'location' => 'St. Gallerhof Zürich',
+'deleted' => '0',
+],
+
+[
+'id' => 3,
+'title' => 'Stammtisch4',
+'date' => '2023-01-05',
+'time' => '18:00',
+'location' => 'St. Gallerhof Zürich',
+'deleted' => '0',
 ],
 
 ];

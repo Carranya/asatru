5efb5d73a7982b800ba6f4cab6fff66a67983513
Carranya <k.gong82@gmail.com>
2022-12-24 21:57:20 +0100

    add edit functions events

--- adminPage/eventList.php
+++ adminPage/eventList.php
@@ -12,14 +12,19 @@
         } else {
             $opacity = 1;
         }
-
-        echo "<div class='flex justify-center m-10'>";
-            echo "<div class='w-96 bg-lime-600 rounded-xl text-center p-5' style='opacity: $opacity'>";
-                echo "<span class='text-xl text-white font-bold'>";
-                echo $event['title'] . "<br>";
-                echo $showDate . "<br>";
-                echo $event['location'] . "<br>";
-                echo "</span>";
+        if(@$_POST['pick'] == $event['id']){
+            include 'adminPage/editEvent.php';
+        } else {
+            echo "<div class='flex justify-center m-10'>";
+                echo "<div class='w-96 bg-lime-600 rounded-xl text-center p-5' style='opacity: $opacity'>";
+                    echo "<span class='text-xl text-white font-bold'>";
+                        echo $event['title'] . "<br>";
+                        echo $showDate . "<br>";
+                        echo $event['location'] . "<br>";
+                    echo "</span>";
+                    echo "<button name='pick' value=" . $event['id'] . " class='m-3 hover:opacity-40'><img src='img/icons/edit.png' class='w-10 h-10'></button>";
+                    echo "<button name='delete' value=" . $event['id'] . " class='m-3 hover:opacity-40' onclick=\"return confirm('Are you sure?');\"><img src='img/icons/delete.png' class='w-10 h-10'></button>";
+                echo "</div>";
             echo "</div>";
-        echo "</div>";
+        }
     }

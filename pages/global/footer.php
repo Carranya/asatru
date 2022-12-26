a66bd3a16a315dfd45cd7895dafc950671966333
Carranya <k.gong82@gmail.com>
2022-12-26 21:10:09 +0100

    add footer

---
+++ pages/global/footer.php
@@ -0,0 +1,15 @@
+<div class='flex justify-center bg-lime-600 text-white items-center'>
+    <div class='m-5'>
+        Impressum<br>
+        FAQ
+    </div>
+    <div class='m-5'>Kontakt:<br>
+        <div class='flex'>
+            <a href='mailto:asatruSchweiz@gmx.ch'><img src='img/icons/mail.png' class='w-7 h-7 hover:opacity-40'></a>
+            <a href='https://www.facebook.com/AsatruSchweiz/' target='_blank'><img src='img/icons/fb.png' class='w-7 h-7 hover:opacity-40'></a>
+        </div>
+    </div>
+    <div class='m-5'>
+        <img src='img/icons/donate.png' class='w-32'>
+    </div>
+</div>

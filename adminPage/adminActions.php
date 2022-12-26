0211edb54ecbd676629c2483f894f6084343c15e
Carranya <k.gong82@gmail.com>
2022-12-26 21:50:28 +0100

    add demo functions

--- adminPage/adminActions.php
+++ adminPage/adminActions.php
@@ -1,36 +1,49 @@
 <?php
 
 if(isset($_POST['editEvent'])){
-    global $events;
-    $id = $_POST['editEvent']-1;
-    $events[$id] = [
-        'id' => $_POST['editEvent'],
-        'title' => $_POST['editTitle'],
-        'date' => $_POST['editDate'],
-        'time' => $_POST['editTime'],
-        'location' => $_POST['editLocation'],
-        'deleted' => $events[$id]['deleted'],
-    ];
-    saveEvent();
+    if($demo == true){
+        include 'adminPage/demo_alert.php';
+    } else {
+
+        global $events;
+        $id = $_POST['editEvent']-1;
+        $events[$id] = [
+            'id' => $_POST['editEvent'],
+            'title' => $_POST['editTitle'],
+            'date' => $_POST['editDate'],
+            'time' => $_POST['editTime'],
+            'location' => $_POST['editLocation'],
+            'deleted' => $events[$id]['deleted'],
+        ];
+        saveEvent();
+    }
 }
 
 if(isset($_POST['createEvent'])){
-    global $events;
-    $id = count($events)+1;
-    $events[] = [
-        'id' => $id,
-        'title' => $_POST['title'],
-        'date' => $_POST['date'],
-        'time' => $_POST['time'],
-        'location' => $_POST['location'],
-        'deleted' => 'false',
-    ];
-    saveEvent();
+    if($demo == true){
+        include 'adminPage/demo_alert.php';
+    } else {
+        global $events;
+        $id = count($events)+1;
+        $events[] = [
+            'id' => $id,
+            'title' => $_POST['title'],
+            'date' => $_POST['date'],
+            'time' => $_POST['time'],
+            'location' => $_POST['location'],
+            'deleted' => 'false',
+        ];
+        saveEvent();
+    }
 }
 
 if(isset($_POST['delete'])){
-    global $events;
-    $id = $_POST['delete']-1;
-    $events[$id]['deleted'] = 'true';
-    saveEvent();
+    if($demo == true){
+        include 'adminPage/demo_alert.php';
+    } else {
+        global $events;
+        $id = $_POST['delete']-1;
+        $events[$id]['deleted'] = 'true';
+        saveEvent();
+    }
 }

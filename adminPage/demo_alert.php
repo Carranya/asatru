0b7fb1adc547baee8f5cb208ce4715141c46631f
Carranya <k.gong82@gmail.com>
2022-12-26 21:50:31 +0100

    add demo functions

---
+++ adminPage/demo_alert.php
@@ -0,0 +1,6 @@
+<div class='flex justify-center'>
+    <div class='text-xl bg-red-600 p-5 m-5 rounded-xl text-center w-80'>
+        <span class='font-bold'>Demo Version</span><br>
+        Funktion nicht verfügbar.
+    </div>
+</div>

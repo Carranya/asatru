29347748ea7c4601783d046a0476a8c87520c1ac
Carranya <k.gong82@gmail.com>
2022-12-26 21:50:39 +0100

    add demo functions

---
+++ adminPage/loginFailed.php
@@ -0,0 +1,5 @@
+<div class='flex justify-center m-10'>
+    <div class='w-96 bg-red-600 rounded-xl text-center p-5'>
+        <span class='text-white font-bold text-xl'>Fehler! Passwort ungültig!</span>
+    </div>
+</div>

ea4116f597d8f8e7947b8f7835cb532022b1abc1
Carranya <k.gong82@gmail.com>
2022-12-26 21:10:06 +0100

    add footer

--- pages/verein.php
+++ pages/verein.php
@@ -1,1 +1,13 @@
-Verein
+<h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-5 mb-5 p-2'>Verein Asatru Schweiz</h1>
+    Seit dem 19.09.2015 ist Asatru Schweiz ein Verein!<br>
+    Wir haben uns nach über 5 Jahren als Gruppe entschlossen, diesen Weg zu gehen.<br>
+    Mit dem Vereinsstatus haben wir nun klar definierte Entscheidungsstrukturen auf demokratischer Basis und als juristische Person andere Handlungsmöglichkeiten. Ferner definieren wir uns nun offiziell als Glaubensgemeinschaft.<br>
+    Die Stammtische bleiben was sie waren, offen für alle!<br>
+    Und auch die Teilnahme an den Jahreskreisfeste ist weiterhin nicht an eine Mitlgliedschaft gebunden.<br>
+    Trotzdem freuen wir uns natürlich über neue Mitglieder in unserem Verein.<br>
+<br>
+<h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-5 mb-5 p-2'>Unser Selbstverständnis</h1>
+    Wir verstehen uns als religiöse Glaubensgemeinschaft, mit dem Ziel die Ausübung, das Erleben und Verständnis des heidnischen, germanischen Glaubens sowie  die Weiterentwicklung von diesem in die heutige Zeit zu fördern.<br>
+    Die persönlichen Glaubensvorstellungen und Praktiken der einzelnen Mitglieder, unterliegen nicht der Beurteilung des Vereins oder seiner Kritik. Sie sind Privatsache.<br>
+    Auch begegnen wir anderen Glaubensüberzeugungen mit Respekt  und Toleranz, denn nach unserer Überzeugung ist religiöses Erleben eine individuelle und keine universelle Wahrheit.<br>
+<br>

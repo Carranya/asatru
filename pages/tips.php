2f87808060752b310761b469cf1a29914356209f
Carranya <k.gong82@gmail.com>
2022-12-27 20:34:56 +0100

    upload

--- pages/tips.php
+++ pages/tips.php
@@ -1,1 +1,24 @@
-Tips
+<h1 class='font-bold text-lg text-white bg-gradient-to-r from-lime-600 to-lime-800 text-center border-2 border-solid border-white mt-5 mb-5 p-2'>Empfehlenswerte Links</h1>
+<div class='flex justify-center'>
+    <img src='img/tips/tips1.jpg' class='w-[400px]'>
+</div>
+<br>
+<div class='flex justify-center'>
+    <img src='img/tips/tips2.gif' class='w-[400px]'>
+</div>
+<br>
+<div class='flex justify-center'>
+    <img src='img/tips/tips3.jpg' class='w-[400px]'>
+</div>
+<br>
+<div class='flex justify-center'>
+    <img src='img/tips/tips4.gif' class='w-[400px]'>
+</div>
+<br>
+<div class='flex justify-center'>
+    <img src='img/tips/tips5.jpg' class='w-[400px]'>
+</div>
+<br>
+<div class='flex justify-center'>
+    <img src='img/tips/tips6.bmp' class='w-[400px]'>
+</div>

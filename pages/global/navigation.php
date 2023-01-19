b86e127fcebfe18e14c8de06f106b5fae2ffc1c4
Carranya <k.gong82@gmail.com>
2023-01-19 10:39:37 +0100

    fixed bugs

--- pages/global/navigation.php
+++ pages/global/navigation.php
@@ -4,18 +4,18 @@
     <div class='md:flex justify-center items-center bg-gradient-to-r from-lime-600 to-lime-800 text-white font-bold text-center'>
         <div id='menuHome' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=home'><span class='pl-5 pr-5'>Home</span></a></div>
         <div id='menuGlaube' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=glaube'><span class='pl-5 pr-5'>Unser Glaube</span></a>
-            <!-- <div id='subGlaube' class='hidden text-center'>
+            <div id='subGlaube' class='hidden text-center'>
                 <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=glaube#goetter'> Unsere Götter</a></div>
                 <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=glaube#naturreligion'>Naturreligion</a></div>
-            </div> -->
+            </div>
         </div>
         <div id='menuTips' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=tips'><span class='pl-5 pr-5'>Tips & Links</span></a></div>
         <div id='menuHistorie' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=historie'><span class='pl-5 pr-5'>Historisches</span></a>
-            <!-- <div id='subHistorie' class='hidden text-center'>
+            <div id='subHistorie' class='hidden text-center'>
                 <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#alemannen'>Alemannen in CH</a></div>
                 <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#gallus'>Die Religion der heid...</a></div>
                 <div class='pt-3 pb-3 pl-5 pr-5 hover:bg-lime-500'><a href='index.php?page=historie#arch'>Archäologische Funde</a></div>
-            </div> -->
+            </div>
         </div>
         <div id='menuVerein' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=verein'><span class='pl-5 pr-5'>Verein Asatru Schweiz</span></a></div>
         <div id='menuStammtisch' class='pt-3 pb-3 hover:bg-lime-600'><a href='index.php?page=stammtisch'><span class='pl-5 pr-5'>Stammtisch</span></a></div>

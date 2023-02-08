e852fde19069fa3207ac741a762af4ed835f91f6
Carranya <k.gong82@gmail.com>
2023-02-08 16:28:25 +0100

    add title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
+    <title>Asatru Schweiz</title>
     <link rel="stylesheet" href="css/output.css">
     <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="js/navigation.js"></script>
